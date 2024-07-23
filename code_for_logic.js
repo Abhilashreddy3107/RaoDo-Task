132bec3281c888dc9c0439ce154533d836ed8a35
Abhilash Reddy Kallepu <110215048+Abhilashreddy3107@users.noreply.github.com>
2024-07-23 09:46:11 +0530

    Create code_for_logic.js

---
+++ code_for_logic.js
@@ -0,0 +1,69 @@
+function calculateMonthlyUsers(users) {
+    const monthlyLoggedInUsers = {};
+    const monthlyActiveUsers = {};
+
+    users.forEach(user => {
+        const loggedInDate = new Date(user.logged_in);
+        const loggedOutDate = new Date(user.logged_out);
+        const lastOpenedDate = new Date(user.lastOpenedAt);
+
+        const loggedInYearMonth = `${loggedInDate.getFullYear()}-${loggedInDate.getMonth() + 1}`;
+        const loggedOutYearMonth = `${loggedOutDate.getFullYear()}-${loggedOutDate.getMonth() + 1}`;
+        const lastOpenedYearMonth = `${lastOpenedDate.getFullYear()}-${lastOpenedDate.getMonth() + 1}`;
+
+        // Track logged-in users per month
+        if (!monthlyLoggedInUsers[loggedInYearMonth]) {
+            monthlyLoggedInUsers[loggedInYearMonth] = new Set();
+        }
+        monthlyLoggedInUsers[loggedInYearMonth].add(user.userId);
+
+        // If logged out is in a different month, track across months
+        if (loggedOutYearMonth !== loggedInYearMonth) {
+            let tempDate = new Date(loggedInDate);
+            while (tempDate <= loggedOutDate) {
+                const tempYearMonth = `${tempDate.getFullYear()}-${tempDate.getMonth() + 1}`;
+                if (!monthlyLoggedInUsers[tempYearMonth]) {
+                    monthlyLoggedInUsers[tempYearMonth] = new Set();
+                }
+                monthlyLoggedInUsers[tempYearMonth].add(user.userId);
+                tempDate.setMonth(tempDate.getMonth() + 1);
+            }
+        }
+
+        // Track active users per month
+        if (!monthlyActiveUsers[lastOpenedYearMonth]) {
+            monthlyActiveUsers[lastOpenedYearMonth] = new Set();
+        }
+        monthlyActiveUsers[lastOpenedYearMonth].add(user.userId);
+    });
+
+    const monthlyMetrics = Object.keys(monthlyLoggedInUsers).map(month => {
+        return {
+            month,
+            loggedInUsers: monthlyLoggedInUsers[month].size,
+            activeUsers: monthlyActiveUsers[month] ? monthlyActiveUsers[month].size : 0
+        };
+    });
+
+    return monthlyMetrics;
+}
+
+// Example usage
+const users = [
+    {
+        deviceId: 'device1',
+        userId: 'user1',
+        logged_in: '2023-01-15',
+        logged_out: '2023-03-10',
+        lastOpenedAt: '2023-02-05'
+    },
+    {
+        deviceId: 'device2',
+        userId: 'user2',
+        logged_in: '2023-02-01',
+        logged_out: '2023-04-15',
+        lastOpenedAt: '2023-03-20'
+    }
+];
+
+console.log(calculateMonthlyUsers(users));
